32640a4a34a54bb800a4c10b9bd4fb6a79d1c1dd
Emily <emilyhannahfountain@gmail.com>
2018-12-09 15:39:55 -0500

    fixed ari

--- smog.py
+++ smog.py
@@ -4,20 +4,22 @@ def run_smog():
     asdfasdflkj
 
 def run_ari(full_text_tokenized):
+    PUNCTUATION = [",", ".", ":", ";", "!", "?"]
     total_sentences = 0
     words_in_sentences = 0
     letters_in_words = 0
     for sent in full_text_tokenized:
         total_sentences += 1
         for word in sent:
-            words_in_sentences += 1
-            letters_in_words += (len(word))
-    print(total_sentences)
-    print(words_in_sentences)
-    print(letters_in_words)
+            if (word not in PUNCTUATION):
+                words_in_sentences += 1
+                letters_in_words += (len(word))
+    # print(total_sentences)
+    # print(words_in_sentences)
+    # print(letters_in_words)
     average_words = words_in_sentences / total_sentences
     average_letters = letters_in_words / words_in_sentences
-    print(average_words)
-    print(average_letters)
-    ar_index = (average_words * 4.71) + (average_letters * 0.5) - 21.43
+    # print(average_words)
+    # print(average_letters)
+    ar_index = (average_letters * 4.71) + (average_words * 0.5) - 21.43
     return ar_index
